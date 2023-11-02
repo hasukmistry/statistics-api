96426f62a918feb85ba28e00684bbaab2c2b79e0
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2023-11-02 13:05:02 +0530

    Update Version20220306163817.php

--- app/migrations/Version20220306163817.php
+++ app/migrations/Version20220306163817.php
@@ -20,15 +20,13 @@ final class Version20220306163817 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, hotel_id INT NOT NULL, score DOUBLE PRECISION NOT NULL, comment VARCHAR(255) DEFAULT NULL, created_date DATETIME NOT NULL, INDEX IDX_794381C63243BB18 (hotel_id), PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C63243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
+        $this->addSql('CREATE TABLE hotel (id INT PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)');
+        $this->addSql('CREATE TABLE review (id INT PRIMARY KEY AUTOINCREMENT, hotel_id INT NOT NULL, score DOUBLE PRECISION NOT NULL, comment VARCHAR(255) DEFAULT NULL, created_date DATETIME NOT NULL, FOREIGN KEY (hotel_id) REFERENCES hotel (id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C63243BB18');
         $this->addSql('DROP TABLE hotel');
         $this->addSql('DROP TABLE review');
     }
