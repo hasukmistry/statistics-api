7643b6709c7ab4a1dd12509279f6873529bc09ad
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2023-11-02 12:59:12 +0530

    Update Version20220306163817.php

--- app/migrations/Version20220306163817.php
+++ app/migrations/Version20220306163817.php
@@ -20,8 +20,8 @@ final class Version20220306163817 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, hotel_id INT NOT NULL, score DOUBLE PRECISION NOT NULL, comment VARCHAR(255) DEFAULT NULL, created_date DATETIME NOT NULL, INDEX IDX_794381C63243BB18 (hotel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, hotel_id INT NOT NULL, score DOUBLE PRECISION NOT NULL, comment VARCHAR(255) DEFAULT NULL, created_date DATETIME NOT NULL, INDEX IDX_794381C63243BB18 (hotel_id), PRIMARY KEY(id))');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C63243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
     }
 
